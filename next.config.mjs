b217b4edb933f8f32d8aa2327e3c0694a80564a4
hadisinjab <hadi.sinjab.00@gmail.com>
2026-01-08 02:11:52 +0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-const createNextIntlPlugin = require('next-intl/plugin');
+import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
@@ -14,6 +14,6 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: false,
   },
-}
+};
 
-module.exports = withNextIntl(nextConfig)
+export default withNextIntl(nextConfig);
