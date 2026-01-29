a79875398ec7aa5efd70c6e52941a7471cb8a46a
hadisinjab <hadi.sinjab.00@gmail.com>
2026-01-29 14:23:43 +0300

    clean code

--- supabase/migrations/update_assignments_constraint.sql
+++
@@ -1,7 +0,0 @@
--- Drop the existing constraint
-ALTER TABLE assignments DROP CONSTRAINT IF EXISTS assignments_type_check;
-
--- Add the updated constraint including 'video_upload'
-ALTER TABLE assignments 
-ADD CONSTRAINT assignments_type_check 
-CHECK (type IN ('text_only', 'text_and_links', 'video_upload'));
